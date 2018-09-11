2c50792daf4c8f3493bf1447300cd5910a41ddcb
YuaCC <1095192336@qq.com>
2018-09-11 22:25:06 +0800

    Update NotifyBinder.java

--- app/src/main/java/com/calendar/b150416/calendar/Notify/NotifyBinder.java
+++ app/src/main/java/com/calendar/b150416/calendar/Notify/NotifyBinder.java
@@ -11,39 +11,12 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class NotifyBinder extends Binder {
-    HashMap<Event,Timer> timers;
-    NotifyManager nm;
+   
     public NotifyBinder(){
-        timers=new HashMap<>();
-        nm=new NotifyManagerAdapter();
     }
     public void addEvent(Event e){
-        if(!timers.containsKey(e)){
-            Calendar cal=Calendar.getInstance();
-            cal.set(Calendar.YEAR,e.year);
-            cal.set(Calendar.MONTH,e.month);
-            cal.set(Calendar.DAY_OF_MONTH,e.day);
-            cal.set(Calendar.HOUR_OF_DAY,e.hour);
-            cal.set(Calendar.MINUTE,e.minute);
-            cal.set(Calendar.SECOND,0);
-            Date date=cal.getTime();
-            Date now=new Date();
-            if(now.compareTo(date)<0){
-                final Event finale=e;
-                Timer timer=new Timer();
-                timer.schedule(new TimerTask() {
-                    @Override
-                    public void run() {
-                        nm.notifyEvent(finale);
-                    }
-                }, date);
-                timers.put(e,timer);
-            }
-        }
+       
     }
     public void removeEvent(Event e){
-        if(timers.containsKey(e)){
-            timers.remove(e);
-        }
     }
 }
